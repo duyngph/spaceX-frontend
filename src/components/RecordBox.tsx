413e716f1678184c27a5bec285043dad4f931685
Duy Nguyen <duyngph@gmail.com>
2021-09-18 12:04:58 +0700

    record feature added

--- src/components/RecordBox.tsx
+++ src/components/RecordBox.tsx
@@ -1,8 +1,72 @@
 import styled from '@emotion/styled'
 import { Button , Box } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import { useReactMediaRecorder } from "react-media-recorder"
 
 const RecordBox = () => {
+    const [second, setSecond] = useState("00");
+    const [minute, setMinute] = useState("00");
+    const [isActive, setIsActive] = useState(false);
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout;
+    
+        if (isActive) {
+          intervalId = setInterval(() => {
+            const secondCounter = counter % 60;
+            const minuteCounter = Math.floor(counter / 60);
+    
+            let computedSecond:any =
+              String(secondCounter).length === 1
+                ? `0${secondCounter}`
+                : secondCounter;
+            let computedMinute:any =
+              String(minuteCounter).length === 1
+                ? `0${minuteCounter}`
+                : minuteCounter;
+    
+            setSecond(computedSecond);
+            setMinute(computedMinute);
+    
+            setCounter((counter) => counter + 1);
+          }, 650);
+        }
+    
+        return () => clearInterval(intervalId);
+      }, [isActive, counter]);
+
+      function stopTimer() {
+        setIsActive(false);
+        setCounter(0);
+        setSecond("00");
+        setMinute("00");
+      }
+    
+      const {
+        startRecording,
+        stopRecording,
+        pauseRecording,
+        mediaBlobUrl
+      } = useReactMediaRecorder({
+        video: false,
+        audio: true,
+      });
+      console.log("deed", mediaBlobUrl);
+
+    // const convertFileToBase64 = (file:any) =>
+    // new Promise((resolve, reject) => {
+    //   const reader = new FileReader();
+    //   reader.readAsDataURL(file.mediaBlobUrl);
+
+    //   reader.onload = () =>
+    //     resolve({
+    //       fileName: file.title,
+    //       base64: reader.result
+    //     });
+    //   reader.onerror = reject;
+    // });
+
     return (
         <Box sx={{
             background: "#fff",
@@ -12,18 +76,42 @@ const RecordBox = () => {
             padding: "20px"
         }}>
             <Box display="flex" alignItems="center" height="100%" justifyContent="space-between">
-                <Box width="400px" border="1px solid #333" height="100%" borderRadius="5px">
-
-                </Box>
                 <Box>
-                    <RecordButton variant="contained">
-                        Record Now
-                    </RecordButton>
-                    <RecordButton variant="contained" color="error" disabled>
-                        Stop
-                    </RecordButton>
+                    <audio src={mediaBlobUrl} controls loop />
                 </Box>
-            </Box>
+                <Box display="flex" alignItems="center">
+                    <TimeBox marginRight="20px">
+                      <span className="minute">{minute}</span>
+                      <span>:</span>
+                      <span className="second">{second}</span>
+                    </TimeBox>
+                    <RecordButton 
+                      variant="contained"
+                      color={
+                        isActive === true ? "error" : "success"
+                      } 
+                      onClick={() => {
+                        if (!isActive) {
+                          startRecording()
+                        } else {
+                          pauseRecording()
+                          stopRecording()
+                          stopTimer()
+                        }
+
+                        setIsActive(!isActive);
+                      }}
+                    >
+                        {isActive ? "Stop" : "Start"}
+                    </RecordButton>
+                    {/* <RecordButton 
+                    variant="contained" color="primary"  
+                    onClick={stopTimer}
+                    >
+                        Reset
+                    </RecordButton> */}
+                </Box>
+                </Box>
         </Box>
     )
 }
@@ -34,4 +122,10 @@ const RecordButton = styled(Button)`
     font-family: inherit;
     margin-right: 10px
 `
+const TimeBox =styled(Box)`
+  span {
+    font-size: 40px
+  }
+`
+
 export default RecordBox
